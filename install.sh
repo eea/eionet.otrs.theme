8f0b769ee90f1d6b9cb03406a8bf09f53c222c12
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-10-28 12:17:25 +0200

    Refs #17077 Common; Added EIONET theme for otrs

--- install.sh
+++ install.sh
@@ -8,8 +8,12 @@ OTRS_USER="otrs"
 OTRS_GROUP="apache"
 OTRS_ROOT="/opt/otrs"
 
+#make theme directory
+/bin/mkdir -p $OTRS_ROOT/Kernel/Output/HTML/EIONET
+
 #Fix permissions and make the proper symlinks
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
+/bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/EIONET
 /bin/chmod -R 2775 $SKIN_PATH/skins/Agent/eionet
 /bin/chmod -R 2775 $SKIN_PATH/skins/Customer/eionet
 
@@ -17,8 +21,10 @@ OTRS_ROOT="/opt/otrs"
 /bin/ln -s $SKIN_PATH/skins/Agent/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
 /bin/ln -s $SKIN_PATH/skins/Customer/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
 
+/bin/cp -r $SKIN_PATH/theme/EIONET $OTRS_ROOT/Kernel/Output/HTML
+
 echo ""
 echo "==========================================================="
-echo "All set. Now you can use the eionet skin in otrs"
+echo "All set. Now you can use the eionet skin and theme in otrs"
 echo "==========================================================="
 echo ""
