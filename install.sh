dfe94ea97c6c6f44e2ac85f10f5ea895661ee3fc
Olimpiu Rob <olimpiu.rob@gmail.com>
2013-12-19 12:09:41 +0200

    Refs #17077; Permissions update

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 #The following variables are user configurable
 SKIN_PATH=`/bin/pwd`
 OTRS_USER="otrs"
-OTRS_GROUP="apache"
+OTRS_GROUP="www-data"
 OTRS_ROOT="/opt/otrs"
 
 #make theme directory
@@ -14,12 +14,15 @@ OTRS_ROOT="/opt/otrs"
 #Fix permissions and make the proper symlinks
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $SKIN_PATH
 /bin/chown -R $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Output/HTML/EIONET
-/bin/chmod -R 2775 $SKIN_PATH/skins/Agent/eionet
-/bin/chmod -R 2775 $SKIN_PATH/skins/Customer/eionet
+/usr/bin/find $SKIN_PATH/skins/ -type f -print0 | xargs -0 /bin/chmod 0660
+/usr/bin/find $SKIN_PATH/skins/ -type d -print0 | xargs -0 /bin/chmod 2770
 
 /bin/ln -s $SKIN_PATH/EionetSkin.xml $OTRS_ROOT/Kernel/Config/Files/ || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/Kernel/Config/Files/EionetSkin.xml
 /bin/ln -s $SKIN_PATH/skins/Agent/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Agent || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Agent/eionet
 /bin/ln -s $SKIN_PATH/skins/Customer/eionet $OTRS_ROOT/var/httpd/htdocs/skins/Customer || true
+/bin/chown -h $OTRS_USER:$OTRS_GROUP $OTRS_ROOT/var/httpd/htdocs/skins/Customer/eionet
 
 /bin/cp -r $SKIN_PATH/theme/EIONET $OTRS_ROOT/Kernel/Output/HTML
 
